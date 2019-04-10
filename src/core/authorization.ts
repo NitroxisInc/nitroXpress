8ef67257345b34dd6500ead8f590d3c997863be9
Hammad Asif <mrhammadasif@gmail.com>
2019-04-10 18:45:04 +0500

    update authorization for API and Web functions

--- src/core/authorization.ts
+++ src/core/authorization.ts
@@ -1,4 +1,3 @@
-import { IModels } from "../models/models"
 import * as express from "express"
 import * as mongoose from "mongoose"
 import UserModel, { Roles } from "../models/user"
@@ -7,9 +6,11 @@ import * as _ from "lodash"
 import * as jwt from "jsonwebtoken"
 import * as fs from "fs"
 import * as path from "path"
+import myLanguage from "../language"
 import { AuthError, Reason, GenerateResp, StatusCode } from "../core/common-errors"
 
-const privateFile = fs.readFileSync(path.join(__dirname, "/../../localhost.key"), {encoding: "utf8"})
+const privateFile = process.env.SECRET
+
 const algo = "HS512"
 
 declare global {
@@ -17,36 +18,124 @@ declare global {
     interface Request {
       user: {
         _id: string
-        name?: string
-        email?: string
+        email: string
         token?: string
-        type?: string
         profile?: any
+        type?: Roles
       }
     }
   }
 }
 
+export function signJwt(objToSign: object, expiresIn: string | null = null) {
+  const jwtOptions: any = {
+    algorithm: algo
+  }
 
-export function signJwt(userObj: object) {
-  return jwt.sign(userObj, privateFile, {
-    algorithm: algo,
-    expiresIn: "7d"
-  })
+  if (expiresIn) jwtOptions.expiresIn = expiresIn
+  return jwt.sign(objToSign, privateFile, jwtOptions)
 }
 
+export function ifLoggedInThenRedirect(elsePath: string = "/") {
+  return (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    if (!req.session.token) {
+      next()
+    } else res.redirect(elsePath)
+  }
+}
+
+export function authorizeByRoleElseRedirect(failurePath: string, ...rolesEnum: Roles[]) {
+  return (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    let token: string = req.header("Authorization") || req.session.token || req.body.token || req.query.token || ""
+    token = typeof token !== "undefined" && token.length > 0 ? token.trim() : token
+    let roles: string[] = []
+    if (token) {
+      // valid type of authorization is provided
+      jwt.verify(token, privateFile, { algorithms: [algo] }, (err, _doc: any) => {
+        if (err) {
+          req.session.errors = (req.session.errors || []).concat(err.message || err).concat(err.message || err)
+          return req.session.save(() => {
+            res.redirect(failurePath)
+          })
+        }
+
+        _.each(rolesEnum, e => {
+          roles.push(Roles[e])
+        })
+
+        // log.info(roles)
+        if (_doc) {
+          UserModel.findOne({
+            _id: _doc._b,
+            password: decrypt(_doc._a),
+            type: { $in: roles }
+          })
+            .then((userObj: any) => {
+              if (userObj) {
+                if (!_.hasIn(userObj, "type") || _.indexOf(roles, userObj.type) < 0) {
+                  req.session.errors = (req.session.errors || []).concat(myLanguage.userForbidden)
+                  return req.session.save(() => {
+                    res.status(StatusCode.forbidden)
+                    res.redirect(failurePath)
+                  })
+                } // else {
+                // req.session.errors = (req.session.errors || []).concat(myLanguage.authRequired)
+                // return req.session.save(() => {
+                //   res.status(StatusCode.auth)
+                //   res.redirect(failurePath)
+                // })
+                // }
+
+                if (_.hasIn(userObj, "profile")) {
+                  req.user = { ...req.user, _id: userObj._id, type: userObj.type }
+                  req.user.profile = userObj["profile"] || {}
+                }
+                next()
+              } else {
+                req.session.errors = (req.session.errors || []).concat(myLanguage.authRequired)
+                return req.session.save(() => {
+                  res.status(StatusCode.auth)
+                  res.redirect(failurePath)
+                })
+              }
+            })
+            .catch(err => {
+              log.error(err)
+              req.session.errors = (req.session.errors || []).concat(myLanguage.dbConnection)
+              return req.session.save(() => {
+                res.status(StatusCode.auth)
+                res.redirect(failurePath)
+              })
+            })
+        } else {
+          req.session.errors = (req.session.errors || []).concat(myLanguage.authInvalid)
+          return req.session.save(() => {
+            res.status(StatusCode.auth)
+            res.redirect(failurePath)
+          })
+        }
+      })
+    } else {
+      req.session.errors = (req.session.errors || []).concat(myLanguage.authInvalid)
+      return req.session.save(() => {
+        res.status(StatusCode.auth)
+        res.redirect(failurePath)
+      })
+    }
+  }
+}
 /**
  * middleware to validate the jsonWebToken
  * only supporting token string to be present in jwt token
  */
-export function authorizeByRole(...rolesEnum: Roles[]) {
+export function authorizeByRoleElseStatus(...rolesEnum: Roles[]) {
   return (req: express.Request, res: express.Response, next: express.NextFunction) => {
     let token: string = req.header("Authorization") || req.body.token || req.query.token || ""
     token = typeof token !== "undefined" && token.length > 0 ? token.trim() : token
     let roles: string[] = []
-    if (!!token) {
+    if (token) {
       // valid type of authorization is provided
-      jwt.verify(token, privateFile, {algorithms: [algo]}, (err, _doc: any) => {
+      jwt.verify(token, privateFile, { algorithms: [algo] }, (err, _doc: any) => {
         if (err) {
           if (err.name === "TokenExpiredError") {
             return res.status(StatusCode.auth).json(GenerateResp(err, Reason.authCodeExpired))
@@ -60,48 +149,45 @@ export function authorizeByRole(...rolesEnum: Roles[]) {
 
         // log.info(roles)
         if (_doc) {
-
           UserModel.findOne({
             _id: _doc._b,
             password: decrypt(_doc._a),
-            // type: { $in: roles }
+            type: { $in: roles }
           })
-          .then((userObj: any) => {
-            if (userObj) {
-              if (_.hasIn(userObj, "type")) {
-                if (_.indexOf(roles, userObj.type) < 0) {
-                  return res.status(StatusCode.forbidden).json(GenerateResp("User not allowed to make this request", Reason.notPermitted))
+            .then((userObj: any) => {
+              if (userObj) {
+                if (_.hasIn(userObj, "type")) {
+                  if (_.indexOf(roles, userObj.type) < 0) {
+                    return res
+                      .status(StatusCode.forbidden)
+                      .json(GenerateResp("User not allowed to make this request", Reason.notPermitted))
+                  }
+                } else {
+                  return res
+                    .status(StatusCode.auth)
+                    .json(GenerateResp("Invalid Auth Code found", Reason.invalidAuthCode))
                 }
-              }
-              else {
-                return res.status(StatusCode.auth).json(GenerateResp("Invalid Auth Code found", Reason.invalidAuthCode))
-              }
 
-              if (_.hasIn(userObj, "profile")) {
-                req.user = userObj["profile"] || {}
-                req.user._id = userObj._id
-                req.user.type = userObj.type
+                if (_.hasIn(userObj, "profile")) {
+                  req.user = userObj["profile"] || {}
+                  req.user._id = userObj._id
+                  req.user.type = userObj.type
+                }
+                next()
+              } else {
+                return res.status(401).json(GenerateResp("No User Found with the given detail", Reason.invalidAuthCode))
               }
-              next()
-            }
-            else {
-              return res.status(401).json(GenerateResp("No User Found with the given detail", Reason.invalidAuthCode))
-            }
-          })
-          .catch(err => {
-            log.error(err)
-            return res.status(401).json(GenerateResp(err || err.message, Reason.dbError))
-          })
-
-        }
-        else {
-          return res.status(401).json(GenerateResp("Invalid Authorization Code", Reason.invalidAuthCode))
+            })
+            .catch(err => {
+              log.error(err)
+              return res.status(401).json(GenerateResp(err || err.message, Reason.dbError))
+            })
+        } else {
+          return res.status(401).json(GenerateResp(myLanguage.authInvalid, Reason.invalidAuthCode))
         }
       })
-    }
-    else {
-      return res.status(400).json(GenerateResp("Authorization Code is required for this request", Reason.authCodeMissing))
+    } else {
+      return res.status(400).json(GenerateResp(myLanguage.authRequired, Reason.authCodeMissing))
     }
   }
 }
-
